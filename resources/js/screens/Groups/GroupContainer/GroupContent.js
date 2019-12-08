b24d0ef37bbb7c0bbe21f91bcb27ac03f3e41f55
AnhPham <anhph@talent.ominext.co>
2019-12-08 16:43:48 +0700

    haianh

---
+++ resources/js/screens/Groups/GroupContainer/GroupContent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Menu, Classes, MenuDivider } from "@blueprintjs/core"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch
+} from "react-router-dom";
+
+import ListGroup from './ListGroup'
+import CreateNewGroup from './CreateNewGroup'
+
+class GroupContent extends React.Component {
+
+    handleClick(e) {
+        e.preventDefault()
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/groups/">
+                    <ListGroup/>
+                </Route>
+                <Route path="/groups/Listgroup">
+                    <ListGroup/>
+                </Route>
+                <Route path="/groups/createNewGroup">
+                    <CreateNewGroup/>
+                </Route>
+            </Switch>
+        )
+    }
+}
+
+export default GroupContent
